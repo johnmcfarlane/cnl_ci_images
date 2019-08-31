e9e384b0d38c2254f0a551329d92f51eaf0d5c49
John McFarlane <git@john.mcfarlane.name>
2019-08-31 12:30:41 +0100

    Add GCC-9 etc.
    - move dockerfiles into subdirectory for easier globbing
    - support multiple dockerfiles as input to build.py
    - remove repository, ppa:ubuntu-toolchain-r/test
      - if we need something from there, just use a newer distro release!
    - added Ubuntu 19.04
      - has GCC-9 out of the box
    - Clang-4 needs Ubuntu 16.04, ICEs with 18.04

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cnl_ci_images
-Docker images used to run [CNL](https://github.com/johnmcfarlane/cnl/) integration
+
+Docker images used to perform [CNL](https://github.com/johnmcfarlane/cnl/)
+integration. You are probably here by accident but welcome anyway!
+
+To build everything:
+
+```sh
+./build.py dockerfiles/*
+```
--- build.py
+++ build.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 
-from subprocess import call
+from os.path import basename, dirname
+from subprocess import check_call
+from sys import argv
 
-dockerfiles = ["base-16.04", "base-18.04", "gcc-8", "gcc-7", "gcc-6", "gcc-5", "clang-6.0", "clang-5.0", "clang-4.0", "clang-3.9", "clang-3.8"]
-for dockerfile in dockerfiles:
-    image = "johnmcfarlane/cnl_ci:{}".format(dockerfile)
-    call(["docker", "build", "-f", dockerfile, "--tag", image, "."])
-    call(["docker", "push", image])
+
+def build(filename, path, tag):
+    # Work in cycles of build/push, build/push, build/push.
+    # That way, all the base images are uploaded ready to pull again for each compiler image.
+    check_call(["docker", "build", "-f", filename, "--tag", tag, path])
+    check_call(["docker", "push", tag])
+
+
+if __name__ == "__main__":
+    if len(argv) <= 1:
+        print("Please provide dockerfile(s).")
+        exit(1)
+
+    # Base images are dependency of the other images, so build them first.
+    filenames = sorted(argv[1:], key=lambda arg: 0 if arg.startswith("base") else 1)
+
+    # From full path, extract salient strings
+    plan = [(filename, dirname(filename), "johnmcfarlane/cnl_ci:{}".format(basename(filename))) for filename in
+            filenames]
+
+    for step in plan:
+        print('=' * 70)
+        print("{} -> {}".format(step[0], step[2]))
+        build(*step)
