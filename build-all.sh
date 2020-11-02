430fdf3662b1bab8aafa6afe5a0b4d48f701d4a8
John McFarlane <git@john.mcfarlane.name>
2020-11-02 19:41:42 +0000

    Break 3/* builds apart
    - they're mostly pretty big and slow anyway; no need to risk breaking the
      (memory) bank

--- build-all.sh
+++ build-all.sh
@@ -8,4 +8,7 @@ set -euo pipefail
 # Technically, 3/*-head don't depend on dockerfiles/1 but they are parallel and sloooow!
 ./build.py dockerfiles/2/*
 
-./build.py dockerfiles/3/*
+./build.py dockerfiles/3/clang-10-libstdcpp
+./build.py dockerfiles/3/clang-11
+./build.py dockerfiles/3/clang-head
+./build.py dockerfiles/3/gcc-head
