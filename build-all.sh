c8ff15c8fd1ae55cda4ac3b4ffc6a47d18a908c0
John McFarlane <cnl@john.mcfarlane.name>
2020-11-10 00:39:33 +0000

    The great dockergeddon

--- build-all.sh
+++ build-all.sh
@@ -5,10 +5,8 @@ set -euo pipefail
 # Unless building in the pipeline, dockerfiles/1/* don't depend on dockerfiles/0
 ./build.py dockerfiles/0/* dockerfiles/1/*
 
-# Technically, 3/*-head don't depend on dockerfiles/1 but they are parallel and sloooow!
 ./build.py dockerfiles/2/*
 
-./build.py dockerfiles/3/clang-10-libstdcpp
-./build.py dockerfiles/3/clang-11
-./build.py dockerfiles/3/clang-head
-./build.py dockerfiles/3/gcc-head
+./build.py dockerfiles/3/*
+./build.py dockerfiles/4/*
+./build.py dockerfiles/5/*
